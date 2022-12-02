d3818adae7f66c9559030aab44cdf4237f4900e2
Cedric Gestes <cedric@jots.ai>
2022-12-02 18:43:04 +0100

    support over on mentions :)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fleather/fleather.dart';
 import 'package:fleather_mention/fleather_mention.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_portal/flutter_portal.dart';
 
 void main() => runApp(const MyApp());
 
@@ -39,10 +40,12 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: const MyHomePage(),
+  Widget build(BuildContext context) => Portal(
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(primarySwatch: Colors.blue),
+          home: const MyHomePage(),
+        ),
       );
 }
 
--- lib/src/embed_builder.dart
+++ lib/src/embed_builder.dart
@@ -1,18 +1,61 @@
 import 'package:fleather/fleather.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_portal/flutter_portal.dart';
 
 import 'const.dart';
 import 'options.dart';
 
-Widget? mentionEmbedBuilder(
-    BuildContext context, EmbedNode node, {Function(MentionData)? onTap}) {
+class MentionWidget extends StatefulWidget {
+  final MentionData data;
+  final void Function(MentionData)? onTap;
+
+  const MentionWidget(this.data, this.onTap, {super.key});
+
+  @override
+  State<MentionWidget> createState() => _MentionWidgetState();
+}
+
+class _MentionWidgetState extends State<MentionWidget> {
+  bool hover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() {
+        hover = true;
+      }),
+      onExit: (_) => setState(() {
+        hover = false;
+      }),
+      child: GestureDetector(
+        onTap: () => widget.onTap?.call(widget.data),
+        child: PortalTarget(
+            visible: hover,
+            portalFollower:
+                Container(height: 42, width: 42, color: Colors.amberAccent),
+            anchor: const Aligned(
+              follower: Alignment.topLeft,
+              target: Alignment.bottomLeft,
+              widthFactor: 1,
+              backup: Aligned(
+                follower: Alignment.bottomLeft,
+                target: Alignment.topLeft,
+                widthFactor: 1,
+              ),
+            ),
+            child: Text('${widget.data.trigger}${widget.data.value}')),
+      ),
+    );
+  }
+}
+
+Widget? mentionEmbedBuilder(BuildContext context, EmbedNode node,
+    {Function(MentionData)? onTap}) {
   if (node.value.type == mentionEmbedKey && node.value.inline) {
     try {
       final data = MentionData.fromJson(node.value.data);
-      return GestureDetector(
-        onTap: () => onTap?.call(data),
-        child: Text('${data.trigger}${data.value}'),
-      );
+      return MentionWidget(data, onTap);
     } catch (_) {}
   }
   return null;
